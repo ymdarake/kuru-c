70fb1c7004f1d35013fe3a2bdc92d820ac3e17e3
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-09-19 00:14:34 +0900

    [add]chapter06: user input

---
+++ chapter06.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void example()
+{
+    int data1, data2;
+    scanf("%d,%d", &data1, &data2);
+    printf("%d , %d\n", data1, data2);
+}
+
+int main(void)
+{
+    example();
+    return 0;
+}
