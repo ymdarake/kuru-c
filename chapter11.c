aa9c52de85781f1b2d870bdee1846684f7a8d6f7
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-09-20 23:55:24 +0900

    [add]chapter11: prototype declaration

---
+++ chapter11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sum(int, int);
+
+int main(void)
+{
+    int value = sum(50, 100);
+    printf("%d\n", value);
+    return 0;
+}
+
+int sum(int min, int max)
+{
+    int num;
+    num = (min + max) * (max - min + 1) / 2;
+    return num;
+}
