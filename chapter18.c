c84d8ad50462b8498ad0c96b52d92849545b37de
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-10-28 22:11:49 +0900

    [add]chapter18: macro

---
+++ chapter18.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define PRINTM(X) printf("%d\n", X)
+#define TAX_RATE 0.03
+
+int main(int argc, char const *argv[])
+{
+    int a1 = 100, a2 = 50;
+    PRINTM(a1);
+    PRINTM(a2);
+    return 0;
+}
